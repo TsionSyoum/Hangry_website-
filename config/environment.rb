7cdd5ed9d098646278f223ffb1c3697b5be7af13
TsionSyoum <17TYS@chapin.edu>
2015-07-29 16:44:10 +0000

    update

---
+++ config/environment.rb
@@ -0,0 +1,13 @@
+require 'bundler'
+Bundler.require
+
+require './app/models/model.rb'
+
+#require './app/models/user'
+
+require './app/models/recipe.rb'
+
+
+configure :development do
+  set :database, "sqlite3:db/database.db"
+end
