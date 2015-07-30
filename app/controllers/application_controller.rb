5212a77d6298e6394e9b73c765735ab06604ac30
Fkastenbaum <fskast@gmail.com>
2015-07-30 13:20:00 +0000

    style updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,17 @@ class ApplicationController < Sinatra::Base
   end
   post "/results" do
     @ingredients = INGREDIENTS
-    "HI"
+    @vegetables = params[:vegetables]
+    @meat_poultry_eggs = params[:meat_poultry_eggs]
+    @herbs_and_spices = params[:herbs_and_spices]
+    @grains = params[:grains]
+    @seafood = params[:seafood]
+    @nuts_and_beans = params[:nuts_and_beans]
+    @oils_and_condiments = params[:oils_and_condiments]
+    @dairy = params[:dairy] 
   end
 
-INGREDIENTS = {:vegetables => ["Artichoke", "Arugula", "Asparagus", "Beetroot", "Bell Peppers", "Bok choy", "Broccoli", "Brussels Sprouts", "Butternut Squash", "Cabbage", "Capsicum", "Carrot", "Cauliflower", "Celery", "Chives", "Cucumber", "Daikon", "Eggplant", "Garlic", "Ginger", "Habanero", "Jalapeno", "Kale", "Lettuce" "Mushrooms", "Okra", "Onion", "Parsley", "Parsnip", "Peas", "Peppers", "Potato", "Pumpkin", "Radish", "Rhubarb", "Rutabaga", "Scallion", "Shallot", "Spinach", "Sweet Corn", "Sweet Potato", "Taro", "Tomato", "Turnip", "Wasabi", "Watercress", "White Radish", "Yam", "Zucchini"], 
+INGREDIENTS = {:vegetables => ["Artichoke", "Arugula", "Asparagus", "Avocado", "Beetroot", "Bamboo Shoots", "Bell Peppers", "Bok choy", "Broccoli", "Brussels Sprouts", "Butternut Squash", "Cabbage", "Capsicum", "Carrots", "Cauliflower", "Celery", "Chives", "Cucumber", "Corn","Daikon", "Eggplant", "Garlic", "Ginger", "Habanero", "Jalapeno", "Kale", "Lettuce", "Mushrooms", "Okra", "Onion", "Parsley", "Parsnip", "Peas", "Peppers", "Potato", "Pumpkin", "Radish", "Rhubarb", "Rutabaga", "Scallion", "Shallot", "Spinach", "Sweet Corn", "Sweet Potato", "Taro", "Tomato", "Turnip", "Wasabi", "Watercress", "White Radish", "Yam", "Zucchini"], 
 :meat_poultry_eggs => ["Eggs", "Beef", "Ham", "Lamb", "Pork", "Bacon", "Veal", "Chicken", "Chicken liver", "Duck", "Duck liver", "Gizzards", "Goose", "Goose liver", "Mutton", "Pheasant", "Turkey", "Snails"],
 
 :herbs_and_spices => ["Anise", "Basil", "Bay Leaf", "Bebere", "Bouquet Garni", "Caraway", "Cardamom" , "Cayenne Pepper", "Chervil", "Chives", "Chia Seeds", "Chili Powder", "Cilantro", "Cinnamon", "Cloves", "Coriander", "Cumin", "Curry Leaves" , "Dukkah", "Fennel", "Fenugreek", "Garam Masala", "Garlic Powder", "Ginger", "Gochugaru", "Grains of Paradise", "Lemon Thyme", "Marjoram", "Mint", "Nutmeg", "Oregano", "Old Bay", "Paprika", "Parsley", "Peppercorns" , "Pumpkin Pie Spice Mix", "Rosemary", "Saffron", "Sage", "Sumac", "Tarragon", "Thyme", "Turmeric"],
@@ -29,7 +36,7 @@ INGREDIENTS = {:vegetables => ["Artichoke", "Arugula", "Asparagus", "Beetroot", 
 
 :nuts_and_beans => ["Black-eyed beans", "Black beans", "Chickpeas", "Azuki beans", "Bean sprouts", "Green beans", "Kidney beans’", "Lentils", "lima beans", "Pinto beans", "Peas", "Soy beans", "Hazelnuts", "Pine nuts", "Sunflower seeds", "Mustard seeds", "Poppy seeds", "Sesame seeds", "Peanuts", "Cashew nuts", "Almonds", "Walnuts", "Macadamia nuts", "Pecan nuts", "Pistachios"],
 
-:oils_and_condiments => ["Coconut oil", "Corn oil", "Olive oil", "Palm oil", "Peanut oil", "Sesame oil", "Soybean oil", "Sunflower oil", "Almond oil", "Brazil nut oil", "Cashew oil", "Hazelnut oil", "Macadamia oil", "Pecan oil", "Pine nut oil", "Walnut oil", "Pumpkin seed oil", "Fish oil", "Ketchup", "Mustard", "Mayonnaise", "Soy sauce", "Hot sauce", "BBQ sauce", "Sour cream", "Horseradish", "Tomato Sauce", "Chilli sauce", "Fish sauce", "Maple syrup", "Jam", "Vinegar", "Worcestershire sauce", "Honey", "Chutney", "Pesto", "Teriyaki sauce", "Thai peanut sauce"],
+:oils_and_condiments => ["Coconut oil", "Corn oil", "Olive oil", "Palm oil", "Peanut oil", "Sesame oil", "Soybean oil", "Sunflower oil", "Almond oil", "Brazil nut oil", "Cashew oil", "Hazelnut oil", "Macadamia oil", "Pecan oil", "Pine nut oil", "Walnut oil", "Pumpkin seed oil", "Canola oil", "Fish oil", "Ketchup", "Mustard", "Mayonnaise", "Soy sauce", "Hot sauce", "BBQ sauce", "Sour cream", "Horseradish", "Tomato Sauce", "Chilli sauce", "Fish sauce", "Maple syrup", "Jam", "Vinegar", "Worcestershire sauce", "Honey", "Chutney", "Pesto", "Teriyaki sauce", "Thai peanut sauce", "Salt", "Pepper", "Chipotles in adobo", "Vegetale stock", "Curry powder", "Tomato juice", "Apple cider vinegar", "Chilli paste"],
 
 :dairy =>["Butter", "Buttermilk", "Cheese", "Condensed milk", "Cottage cheese", "Cream", "Cream cheese", "Custard", "Dulce de leche", "Evaporated milk", "Frozen yogurt", "Goat milk", "Ice cream", "Whipped cream", "Whole milk", "Skim milk"]}
 end
